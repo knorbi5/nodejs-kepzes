2eaae137e09fb7a7662c6f1aadfd6b31984f319b
Kulcsár Norbert <knorbi8@gmail.com>
2022-12-12 18:42:31 +0100

    sorrend

--- zarovizsga/app/middlewares/tweets/getTweets.js
+++ zarovizsga/app/middlewares/tweets/getTweets.js
@@ -10,8 +10,8 @@ module.exports = (objRep) => {
             res.locals.username = userModel.findOne({id: req.params.userid}).email;
         }
 
-        // lekérdezzük az összes tweetet
-        res.locals.tweets = tweetModel.find(searchParam);
+        // lekérdezzük az összes tweetet létrehozás szerinti fordított sorrendben (legújabb legelöl)
+        res.locals.tweets = tweetModel.find(searchParam).reverse();
 
         // hozzáfűzzük a tweetekhez tartozó usereket
         const users = userModel.find();
